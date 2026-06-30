e977dea66af95440d9fcbb4dcc115a3aed8abdc2
Kaleru Pranav Kumar <kalerupranav06@gmail.com>
2026-06-30 12:41:28 +0530

    feat(backend): configure database connection and connect on server startup

--- to-do-app/backend/server.js
+++ to-do-app/backend/server.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/connect");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Enable CORS for all HTTP requests
-app.use(express.json());
+app.use(express.json()); //Parse incoming JSON requests
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Todo list backend server is running');
+app.get("/", (req, res) => {
+  res.send("Todo list backend server is running");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Connected to MongoDB successfully...");
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
+  } catch (error) {
+    console.log("Database connection failed: ", error.message);
+  }
+};
+
+start();
