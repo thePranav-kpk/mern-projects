483d8dcd7198ba582dbb0f005197739fed30db6f
Kaleru Pranav Kumar <kalerupranav06@gmail.com>
2026-07-02 00:30:55 +0530

    refactor(backend): replace hardcoded HTTP status codes with http-status-codes package constants

--- to-do-app/backend/controllers/todos.js
+++ to-do-app/backend/controllers/todos.js
@@ -1,45 +1,52 @@
 const Todo = require("../models/Todo");
 const asyncWrapper = require("../middleware/async");
 const { createCustomError } = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
 
 const getAllTodos = asyncWrapper(async (req, res) => {
   const todos = await Todo.find({});
-  res.status(200).json({ todos });
+  res.status(StatusCodes.OK).json({ todos });
 });
 
 const createTodo = asyncWrapper(async (req, res) => {
   const todo = await Todo.create(req.body);
-  res.status(201).json({ todo });
+  res.status(StatusCodes.CREATED).json({ todo });
 });
 
-const getTodo = asyncWrapper(async (req, res) => {
+const getTodo = asyncWrapper(async (req, res, next) => {
   const { id: todoId } = req.params;
   const todo = await Todo.findOne({ _id: todoId });
   if (!todo) {
-    return next(createCustomError(`No todo with id: ${todoId}`, 404));
+    return next(
+      createCustomError(`No todo with id : ${todoId}`, StatusCodes.NOT_FOUND),
+    );
   }
-  res.status(200).json({ todo });
+  res.status(StatusCodes.OK).json({ todo });
 });
 
-const updateTodo = asyncWrapper(async (req, res) => {
+const updateTodo = asyncWrapper(async (req, res, next) => {
   const { id: todoId } = req.params;
   const todo = await Todo.findOneAndUpdate({ _id: todoId }, req.body, {
     new: true,
     runValidators: true,
   });
   if (!todo) {
-    return next(createCustomError(`No todo with id: ${todoId}`, 404));
+    return next(
+      createCustomError(`No todo with id : ${todoId}`, StatusCodes.NOT_FOUND),
+    );
   }
-  res.status(200).json({ todo });
+  res.status(StatusCodes.OK).json({ todo });
 });
 
-const deleteTodo = asyncWrapper(async (req, res) => {
+const deleteTodo = asyncWrapper(async (req, res, next) => {
   const { id: todoId } = req.params;
   const todo = await Todo.findOneAndDelete({ _id: todoId });
   if (!todo) {
-    return next(createCustomError(`No todo with id: ${todoId}`, 404));
+    return next(
+      createCustomError(`No todo with id : ${todoId}`, StatusCodes.NOT_FOUND),
+    );
   }
-  res.status(200).json({ todo });
+  res.status(StatusCodes.OK).json({ todo });
 });
 
 module.exports = {
--- to-do-app/backend/middleware/error-handler.js
+++ to-do-app/backend/middleware/error-handler.js
@@ -1,11 +1,30 @@
+const { StatusCodes } = require("http-status-codes");
 const { CustomAPIError } = require("../errors/custom-error");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // Handle Custom API Errors (e.g. 404 Not Found)
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
+
+  // Handle Mongoose Validation Errors
+  if (err.name === "ValidationError") {
+    const messages = Object.values(err.errors).map((item) => item.message);
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: messages.join(", ") });
+  }
+
+  // Handle Mongoose Cast Errors
+  if (err.name === "CastError") {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: `No item found with id: ${err.value}` });
+  }
+
+  // Fallback generic server error
   return res
-    .status(500)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ msg: "Something went wrong, please try again" });
 };
 
